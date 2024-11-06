0cdc6b0c4b03689cda01fd98ed0547400c076c57
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-06 19:29:51 +0530

    roboshop-shellscripts

--- catalogue.sh
+++ catalogue.sh
@@ -42,11 +42,20 @@ dnf install nodejs -y &>> $LOGFILE
 
 VALIDATE $? "installing nodejs" 
 
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi  
+
 useradd roboshop &>> $LOGFILE
 
 VALIDATE $? "creating roboshop user"
 
-mkdir /app &>> $LOGFILE
+mkdir -p /app &>> $LOGFILE
 
 VALIDATE $? "creating app directory"
 
@@ -56,7 +65,7 @@ VALIDATE $? "downloading catalogue application"
 
 cd /app &>> $LOGFILE
 
-unzip /tmp/catalogue.zip &>> $LOGFILE
+unzip -o /tmp/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? "unzipping catalogue application" 
 
